ced2a3a9c98ff697415fe238ae622dd477911480
Đỗ Hoài Nam <38395309+DoHoaiNam914@users.noreply.github.com>
2022-04-25 13:54:56 +0700

    Update bo-doc.js

--- js/bo-doc.js
+++ js/bo-doc.js
@@ -7,7 +7,7 @@ function loadJNovelClubSpinesContent(book, volume, spineList) {
     let spineName = spineList[i].replace('.xhtml', '');
     xhr.open("GET", "OEBPS/Text/" + spineName.toString().concat('.xhtml'), false);
     xhr.onload = function() {
-      spines += '<div' + (xhr.responseText.includes('<img') ? ' class="nomargin center"' : '') + (!xhr.responseText.includes('id="' + spineName + '"') ? ' id="' + spineName + '"' : '') + '>' + parser.parseFromString(xhr.responseText, 'text/xml').body.innerHTML.replace(/..\/Images\/jnovelclub/g,'https://raw.githubusercontent.com/DoHoaiNam914/CDN/main/tranh-minh-hoa/jnovelclub').replace(/..\/Images/g,'https://raw.githubusercontent.com/DoHoaiNam914/CDN/main/tranh-minh-hoa/' + book +'/' + volume).replace(/..\/Text\//g, '#').replace(/.xhtml/g, '').replace(/\n$/, '').replace(/#toc#/g, '#') + '</div>\n\n\n';
+      spines += '<div' + (xhr.responseText.includes('<img') ? ' class="nomargin center"' : '') + (!xhr.responseText.includes('id="' + spineName + '"') ? ' id="' + spineName + '"' : '') + '>' + parser.parseFromString(xhr.responseText, 'text/xml').body.innerHTML.replace(' xmlns="http://www.w3.org/1999" xmlns:epub="http://www.idpf.org/2007/ops"', '').replace('epub:', '').replace(/..\/Images\/jnovelclub/g,'https://raw.githubusercontent.com/DoHoaiNam914/CDN/main/tranh-minh-hoa/jnovelclub').replace(/..\/Images/g,'https://raw.githubusercontent.com/DoHoaiNam914/CDN/main/tranh-minh-hoa/' + book +'/' + volume).replace(/..\/Text\//g, '#').replace(/.xhtml/g, '').replace(/\n$/, '').replace(/#toc#/g, '#') + '</div>\n\n\n';
       console.log('Loaded:' + spineList[i]);
     }
 
