1d0f6fecf61fae793983b37a19a586fd0c805f01
Đỗ Hoài Nam <38395309+DoHoaiNam914@users.noreply.github.com>
2022-09-08 01:04:59 +0700

    Update DoHoaiNamStyle-after.css, style.css and fourteen other files

--- js/app.js
+++ js/app.js
@@ -19,10 +19,10 @@ $(document).ready(function () {
   $("#background_select").on("change", function () {
     if (this.value != 'white') {
       $(document.documentElement).attr("style", $(document.documentElement).attr("style") != null ? document.documentElement.getAttribute('style').replace(/ black;/g, '').replace(/black;/g, '').replace(/ sepia;/g, '').replace(/sepia;/g, '').replace(/ cream;/g, '').replace(/cream;/g, '').concat(' ' + this.value.concat(';')) : this.value.concat(';'));
-      Cookies.set("background", this.value, { expires: 365 });
+      $.cookie('background', this.value, { expires: 365 });
     } else {
       $(document.documentElement).attr("style", $(document.documentElement).attr("style") != null ? document.documentElement.getAttribute('style').replace(/ black;/g, '').replace(/black;/g, '').replace(/ sepia;/g, '').replace(/sepia;/g, '').replace(/ cream;/g, '').replace(/cream;/g, '') : "");
-      Cookies.remove("background");
+      $.removeCookie('background');
     }
   });
 </script>`);
@@ -40,10 +40,14 @@ function loadYenPressSpinesContent(book, volume, spineList) {
         if (i === 0) {
           $(document.documentElement).attr("lang", html.documentElement.getAttribute('lang'));
           $(document.head).html(html.head.innerHTML.replace(new RegExp(' xmlns="http://www.w3.org/1999/xhtml"', 'g'), '').toString());
-          $("meta[content=\"text/html; charset=UTF-8\"]").replaceWith("<meta charset=\"UTF-8\">\n<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n<meta name=\"viewport\" content=\"initial-scale=1, user-scalable=0, maximum-scale=1\">");
-          $("link[href=\"../Styles/stylesheet.css\"]").replaceWith("<link rel=\"stylesheet\" href=\"../../css/styles/DoHoaiNamStyle-before.css\">\n" + $("link[href=\"../Styles/stylesheet.css\"]").prop("outerHTML").replace('..', volume.concat('/OEBPS')) + "\n<link rel=\"stylesheet\" href=\"../../css/styles/DoHoaiNamStyle-after.css\">");
-          $(document.head).append("<script src=\"https://code.jquery.com/jquery-3.6.0.min.js\" integrity=\"sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=\" crossorigin=\"anonymous\"></script>\n<script src=\"../../js/js.cookie.js\"></script>");
-          $("#background_select").val(Cookies.get("background") || "white").change();
+          $("meta[content=\"text/html; charset=UTF-8\"]").replaceWith(`<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="initial-scale=1, user-scalable=0, maximum-scale=1">`);
+          $(document.head).append(`<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous" script>
+<script src="../../js/jquery.cookie.js"></script>`);
+          $(document.head).append("<link rel=\"stylesheet\" href=\"../../css/styles/DoHoaiNamStyle-before.css\">\n" + $("link[href=\"../Styles/stylesheet.css\"]").prop("outerHTML").replace('..', volume.concat('/OEBPS')) + "\n<link rel=\"stylesheet\" href=\"../../css/styles/DoHoaiNamStyle-after.css\">");
+          $("link[href=\"../Styles/stylesheet.css\"]").remove();
+          $("#background_select").val($.cookie('background') || "white").change();
         }
 
         $(document.body).append("\n<div" + (!this.responseText.includes('id="' + spineName + '"') ? " id=\"" + spineName + "\"" : "") + ">" + html.body.innerHTML.toString().replace(new RegExp(' xmlns="http://www.w3.org/1999/xhtml"', 'g'), '').replace(/epub:/g, '').replace(new RegExp(' xmlns:epub="http://www.idpf.org/2007/ops"', 'g'), '').replace(new RegExp('../Images', 'g'), 'https://raw.githubusercontent.com/DoHoaiNam914/CDN/main/light-novel/' + book.concat('/' + volume.concat('/OEBPS/Images'))).replace(new RegExp('../Text/', 'g'), '#').replace(/.xhtml/g, '').replace(/#cover#/g, '#').replace(/#toc#/g, '#') + "</div>\n\n");
@@ -54,7 +58,11 @@ function loadYenPressSpinesContent(book, volume, spineList) {
     xhr.send();
   }
 
-  window.location.hash = '';
+  if (window.location.hash != null) {
+    window.location.hash = '';
+  } else {
+    window.location.href = '#';
+  }
 }
 
 function loadJNovelClubSpinesContent(book, volume, spineList) {
@@ -67,10 +75,14 @@ function loadJNovelClubSpinesContent(book, volume, spineList) {
         if (i === 0) {
           $(document.documentElement).attr("lang", html.documentElement.getAttribute('lang'));
           $(document.head).html(html.head.innerHTML.replace(new RegExp(' xmlns="http://www.w3.org/1999/xhtml"', 'g'), '').toString());
-          $("meta[content=\"text/html; charset=UTF-8\"]").replaceWith("<meta charset=\"UTF-8\">\n<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n<meta name=\"viewport\" content=\"initial-scale=1, user-scalable=0, maximum-scale=1\">");
-          $("link[href=\"../Styles/stylesheet.css\"]").replaceWith("<link rel=\"stylesheet\" href=\"../../css/styles/DoHoaiNamStyle-before.css\">\n" + $("link[href=\"../Styles/stylesheet.css\"]").prop("outerHTML").replace('..', volume.concat('/OEBPS')) + "\n  <link rel=\"stylesheet\" href=\"../../css/styles/DoHoaiNamStyle-after.css\">");
-          $(document.head).append("<script src=\"https://code.jquery.com/jquery-3.6.0.min.js\" integrity=\"sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=\" crossorigin=\"anonymous\"></script>\n<script src=\"../../js/js.cookie.js\"></script>");
-          $("#background_select").val(Cookies.get("background") || "white").change();
+          $("meta[content=\"text/html; charset=UTF-8\"]").replaceWith(`<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="initial-scale=1, user-scalable=0, maximum-scale=1">`);
+          $(document.head).append(`<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous" script>
+<script src="../../js/jquery.cookie.js"></script>`);
+          $(document.head).append("<link rel=\"stylesheet\" href=\"../../css/styles/DoHoaiNamStyle-before.css\">\n" + $("link[href=\"../Styles/stylesheet.css\"]").prop("outerHTML").replace('..', volume.concat('/OEBPS')) + "\n  <link rel=\"stylesheet\" href=\"../../css/styles/DoHoaiNamStyle-after.css\">");
+          $("link[href=\"../Styles/stylesheet.css\"]").remove();
+          $("#background_select").val($.cookie('background') || "white").change();
         }
 
         $(document.body).append("\n<div" + (this.responseText.includes('<img') ? " class=\"nomargin center\"" : "") + (!this.responseText.includes('id="' + spineName + '"') ? " id=\"" + spineName + "\"" : "") + ">" + html.body.innerHTML.toString().replace(new RegExp(' xmlns="http://www.w3.org/1999/xhtml"', 'g'), '').replace(/epub:/g, '').replace(new RegExp(' xmlns:epub="http://www.idpf.org/2007/ops"', 'g'), '').replace(new RegExp('../Images', 'g'), 'https://raw.githubusercontent.com/DoHoaiNam914/CDN/main/light-novel/' + book.concat('/' + volume.concat('/OEBPS/Images'))).replace(new RegExp('../Text/', 'g'), '').replace(/.xhtml/g, '').replace(/#toc#/g, '#') + "</div>\n\n");
@@ -81,5 +93,9 @@ function loadJNovelClubSpinesContent(book, volume, spineList) {
     xhr.send();
   }
 
-  window.location.hash = '';
+  if (window.location.hash != null) {
+    window.location.hash = '';
+  } else {
+    window.location.href = '#';
+  }
 }
