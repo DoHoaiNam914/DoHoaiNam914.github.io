9679c35eadb37228f515c533820e5a78c8326079
Đỗ Hoài Nam <38395309+DoHoaiNam914@users.noreply.github.com>
2022-09-20 14:55:18 +0700

    :)

--- js/app.js
+++ js/app.js
@@ -143,14 +143,15 @@ function loadJNovelClubSpinesContent(spineList) {
           $("#background_select").val(Cookies.get('background') || "white").change();
         }
 
-        $(document.body).append("\n<div class=\"body" + (this.responseText.includes('<img') ? " nomargin center\"" : "\"") + (!this.responseText.includes('id="' + spineName + '"') ? " id=\"" + spineName + "\"" : "") + ">" +
+        $(document.body).append("\n<div class=\"body" + (this.responseText.includes('<img') ? " nomargin center\"" : "\"") +
+          (!this.responseText.includes('id="' + spineName + '"') ? " id=\"" + spineName + "\"" : "") + ">" +
           html.body.innerHTML.toString().replace(new RegExp(' xmlns="http://www.w3.org/1999/xhtml"', 'g'), '').
-            replace(/epub:/g, '').
-            replace(new RegExp(' xmlns:epub="http://www.idpf.org/2007/ops"', 'g'), '').
-            replace(new RegExp('../Images', 'g'), 'https://raw.githubusercontent.com/DoHoaiNam914/CDN/main/light-novel/' + book.concat('/' + volume.concat('/OEBPS/Images'))).
-            replace(new RegExp('../Text/', 'g'), '').
-            replace(/.xhtml/g, '').
-            replace(/#toc#/g, '#') + "</div>\n\n");
+          replace(/epub:/g, '').
+          replace(new RegExp(' xmlns:epub="http://www.idpf.org/2007/ops"', 'g'), '').
+          replace(new RegExp('../Images', 'g'), 'https://raw.githubusercontent.com/DoHoaiNam914/CDN/main/light-novel/' + book.concat('/' + volume.concat('/OEBPS/Images'))).
+          replace(new RegExp('../Text/', 'g'), '').
+          replace(/.xhtml/g, '').
+          replace(/#toc#/g, '#') + "</div>\n\n");
       }
     };
 
