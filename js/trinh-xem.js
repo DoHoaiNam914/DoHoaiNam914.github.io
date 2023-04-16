05b795f5592985a5eceb7afb47fbab7270b4a8ba
Đỗ Hoài Nam <dohoainam1409@gmail.com>
2023-04-16 20:18:21 +0700

    Update trinh-xem.js

--- js/trinh-xem.js
+++ js/trinh-xem.js
@@ -111,9 +111,9 @@ function j_novelLoader(book, volume, spine) {
       $(document.body).append(`\n<div class="body${
       (data.toString().includes('<img') ?
           ' nomargin center"' : '"') +
-          !data.toString().includes('id="' +
-          spineId + '"') ? ` id="${spineId}"` :
-          ''}>${$(data).find("body").html()}</div>\n\n`);
+          (!data.toString().includes('id="' +
+spineId + '"') ? ` id="${spineId}"` :
+          '')}>${$(data).find("body").html()}</div>\n\n`);
       $("div.body").last().addClass($(data).find("body").attr("class"));
     });
   }
@@ -217,91 +217,6 @@ function yenpressLoader(book, volume, spine) {
 
 function customLoader(book, volume, spine) {
   switch (`${book}_${volume}`) {
-    case 'tinhlinh_22-junpaku-no-houteishiki':
-      for (let i = 0; i < spine.length; i++) {
-        let spineHref = spine[i];
-        let spineId = spineHref.replace(/text\/(.+)\.x?html/, '$1');
-
-        $.get({
-          async: false,
-          crossDomain: false,
-          url: `./${book}/${volume}/${spineHref}`,
-          processData: false
-        }).done(function (data) {
-          let html = !spineHref.includes('xhtml') ? (new DOMParser()).parseFromString(data, 'application/xhtml+xml') : undefined;
-
-          if (i === 5) {
-            $(document.documentElement).attr("lang", $(html.documentElement).attr("xml:lang"));
-            $(document.documentElement).addClass("hltr1");
-            $(document.head).html($(html.head).html());
-            $("meta[content=\"text/html; charset=UTF-8\"]").replaceWith(`<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="initial-scale=1, user-scalable=0, maximum-scale=1">`);
-
-            let stylesheetHref = 
-                $("link[href=\"../stylesheet.css\"]").attr("href").replace('..',
-                `./${book}/${volume}`);
-            let pageStyles = 
-                $("link[href=\"../page_styles.css\"]").prop("outerHTML").replace('..',
-                `./${book}/${volume}`);
-            $("link[href=\"../stylesheet.css\"]").remove();
-            $("link[href=\"../page_styles.css\"]").remove();
-
-            $(document.head).append("<link rel=\"stylesheet\" href=\"/css/styles/trinh-xem-before.css\">");
-            $.get({
-              async: false,
-              crossDomain: false,
-              url: stylesheetHref,
-              processData: false
-            }).done(function (data) {
-              let stylesheet = document.createElement('style');
-
-              stylesheet.type = 'text/css';
-              stylesheet.innerText = data.replace(/(:\s*?.*)serif/g, '$1var(--baseFontFamily)');
-
-              $(document.head).append(stylesheet);
-            });
-
-            $(document.head).append(pageStyles);
-            $(document.head).append("<link rel=\"stylesheet\" href=\"/css/styles/trinh-xem-after.css\">");
-
-            $("#view-select").val(localStorage.getItem("view") || "vertical").change()
-            $LAB.setOptions({AlwaysPreserveOrder:true})
-              .script("/lib/jquery-3.6.1.js")
-              .script("/js/nen.js").wait(() =>
-                  $("#background-select").val(localStorage.getItem("background") ||
-                  "sepia").change());
-          }
-
-          $(document.body).append(`\n<div class="body">${(html != undefined ?
-              $(html.body).html() : $(data).find("body").html()).replace(new
-              RegExp('<rb>(\\p{scx=Hani}+)</rb><rt>\\p{scx=Hira}+</rt>', 'gu'),
-              '$1').replace(/<ruby>(\p{scx=Hani}+)<\/ruby>/gu, '$1').replace(/<rt>/g,
-              '（<rtc>').replace(/<\/rt>/g, '</rtc>）')}</div>\n\n`);
-          $("div.body").last().addClass((html != undefined ?
-              $(html.body) : $(data).find("body")).attr("class"));
-        });
-      }
-
-      $("image").each(function () {
-        $(this).attr("xlink:href",
-            $(this).attr("xlink:href").replace('..',
-            `https://raw.githubusercontent.com/DoHoaiNam914/CDN/main/light-novel/${book}/${volume}`).replace(new RegExp('cover.jpeg', 'g'),
-            `https://raw.githubusercontent.com/DoHoaiNam914/CDN/main/light-novel/${book}/${volume}/cover.jpeg`));
-      });
-
-      $("img").each(function () {
-        $(this).attr("src",
-            $(this).attr("src").replace('..',
-            `https://raw.githubusercontent.com/DoHoaiNam914/CDN/main/light-novel/${book}/${volume}`));
-      });
-
-      $("a[href]").each(function () {
-        $(this).attr("href", $(this).attr("href").replace(/part(.+)\.html/g, ''));
-      });
-
-      break;
-
     case 'tenseislime_20':
       for (let i = 0; i < spine.length; i++) {
         let spineHref = spine[i];
