9727596a545765338389b8d336ca1d1ad48768b3
Đỗ Hoài Nam <dohoainam1409@gmail.com>
2024-01-08 04:31:14 +0700

    Update Translators.js

--- static/js/dich-nhanh/Translators.js
+++ static/js/dich-nhanh/Translators.js
@@ -1047,7 +1047,7 @@ class Vietphrase {
     return inputText.replace(/[’”…、。】！），：？](?![\p{Pc}\p{Pd}\p{Pe}\p{Pf}\p{Po}\s]|$)/gu, (match) => `${PUNCTUATIONS[match] ?? match} `)
       .replace(/([^\s\p{Ps}\p{Pi}])([‘“【（])/gu, (__, p1, p2) => `${p1} ${PUNCTUATIONS[p2] ?? p2}`)
       .replace(/[、。【】！（），：？]/g, (match) => PUNCTUATIONS[match] ?? match)
-      .replace(/ ?· ?/g, '');
+      .replace(/ · /g, ' ');
   }
 
   translatePrioritizeLongVietPhraseClusters(targetLanguage, data, inputText) {
