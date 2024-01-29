1acf2f6d02b544ece4cfe36a3981a3f7fc4019a9
Đỗ Hoài Nam <38395309+DoHoaiNam914@users.noreply.github.com>
2024-01-29 21:18:01 +0700

    Update dichNhanh.js

--- static/js/dich-nhanh/dichNhanh.js
+++ static/js/dich-nhanh/dichNhanh.js
@@ -1492,7 +1492,7 @@ $dropdownHasCollapse.on('hide.bs.dropdown', function onHideBsDropdown() {
 
 $('.define-button').on('click', function onClick() {
   if ($sourceEntryInput.val().length > 0) {
-    window.open($(this).data('href').replace('{0}', encodeURIComponent(($sourceEntryInput.val().substring($sourceEntryInput.prop('selectionStart'), $sourceEntryInput.prop('selectionEnd')) || $sourceEntryInput.val()).trim() /** .substring(0, 30) */)), '_blank', 'popup');
+    window.open($(this).data('href').replace('{0}', encodeURIComponent(($sourceEntryInput.val().substring($sourceEntryInput.prop('selectionStart'), $sourceEntryInput.prop('selectionEnd')) || $sourceEntryInput.val()).trim() /** .substring(0, 30) */)), '_blank', 'width=1000,height=577');
   }
 
   if (window.getSelection) {
@@ -1506,7 +1506,7 @@ $('.define-button').on('click', function onClick() {
 
 $('.translate-webpage-button').on('click', function onClick() {
   if ($sourceEntryInput.val().length > 0) {
-    window.open($(this).data('href').replace('{0}', encodeURIComponent($sourceEntryInput.val().trimEnd())), '_blank', 'popup');
+    window.open($(this).data('href').replace('{0}', encodeURIComponent($sourceEntryInput.val().trimEnd())), '_blank', 'width=1000,height=577');
   }
 
   if (window.getSelection) {
