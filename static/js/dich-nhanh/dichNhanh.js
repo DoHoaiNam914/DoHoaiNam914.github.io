1afb17f176baf21874a36d205de5e258377216de
Đỗ Hoài Nam <38395309+DoHoaiNam914@users.noreply.github.com>
2024-06-23 13:55:24 +0700

    Update dichNhanh.js

--- static/js/dich-nhanh/dichNhanh.js
+++ static/js/dich-nhanh/dichNhanh.js
@@ -392,7 +392,6 @@ var glossaryData = '';
 let currentTranslator = null;
 let translateAbortController = null;
 let prevScrollTop = 0;
-let reloadGlossaryDataTimeout = null;
 let vietPhraseTimeout = null;
 
 let lastSession = {};
@@ -975,10 +974,10 @@ async function translateText(inputText, translatorOption, targetLanguage, glossa
 }
 
 function reloadGlossaryEntries() {
-  clearTimeout(reloadGlossaryDataTimeout);
   const $downloadButton = $('#download-button');
   const $glossaryExtension = $('#glossary-extension');
-  const glossaryList = glossary[$glossaryListSelect.val()];
+  const selectGlossaryList = $glossaryListSelect.val();
+  const glossaryList = glossary[selectGlossaryList];
 
   const glossaryKeys = Object.keys(glossaryList);
 
@@ -1014,56 +1013,54 @@ function reloadGlossaryEntries() {
     },
   });
 
-  let glossaryLength = glossaryKeys.length
+  let glossaryLength = glossaryKeys.length;
 
   if (glossaryLength > 0) {
-    reloadGlossaryDataTimeout = setTimeout(() => {
-      const glossaryDataCopyButton = $copyButtons.filter('[data-target="glossaryData"]');
-      glossaryDataCopyButton.addClass('disabled');
-      $downloadButton.addClass('disabled');
+    const glossaryDataCopyButton = $copyButtons.filter('[data-target="glossaryData"]');
+    glossaryDataCopyButton.addClass('disabled');
+    $downloadButton.addClass('disabled');
 
-      switch ($glossaryTypeSelect.val()) {
-        case GlossaryType.CSV: {
-          glossaryData = $.csv.fromArrays(glossaryEntryList.toSorted((a, b) => b[3] - a[3] || a[1].localeCompare(b[1], 'vi', { ignorePunctuation: true }) || a[0].localeCompare(b[0], 'vi', { ignorePunctuation: true })));
-          $glossaryExtension.text('csv');
-          break;
-        }
-        case GlossaryType.VIETPHRASE: {
-          switch ($vietPhraseType.val()) {
-            case 'QuickTranslate Without Sorting': {
-              glossaryData = `\ufeff${glossaryEntryList.map((element) => element.join('=')).join('\r\n')}`;
-              break;
-            }
-            case 'QuickTranslate': {
-              glossaryData = `\ufeff${glossaryEntryList.toSorted((a, b) => b[2] - a[2] || a[0].localeCompare(b[0])).map((element) => element.join('=')).join('\r\n')}`;
-              break;
-            }
-            case 'Sáng Tác Việt': {
-              glossaryData = glossaryEntryList.toSorted().toSorted((a, b) => a[2] - b[2]).map((element) => element.join('=')).join('\n');
-              break;
-            }
-            default: {
-              glossaryData = glossaryEntryList.map((element) => element.join('=')).join('\n');
-              break;
-            }
-          }
-  
-          $glossaryExtension.text('txt');
-          break;
-        }
-        default: {
-          glossaryData = glossaryEntryList.toSorted((a, b) => b[3] - a[3] || a[1].localeCompare(b[1], 'vi', { ignorePunctuation: true }) || a[0].localeCompare(b[0], 'vi', { ignorePunctuation: true })).map((element) => element.join('\t')).join('\n');
-          $glossaryExtension.text('tsv');
-          break;
-        }
+    switch (['vietPhrase', 'name'].every((element) => selectGlossaryList !== element) ? $glossaryTypeSelect.val() : GlossaryType.VIETPHRASE) {
+      case GlossaryType.CSV: {
+        glossaryData = $.csv.fromArrays(glossaryEntryList.toSorted((a, b) => b[3] - a[3] || a[1].localeCompare(b[1], 'vi', { ignorePunctuation: true }) || a[0].localeCompare(b[0], 'vi', { ignorePunctuation: true })));
+        $glossaryExtension.text('csv');
+        break;
       }
+      case GlossaryType.VIETPHRASE: {
+        switch ($vietPhraseType.val()) {
+          case 'QuickTranslate Without Sorting': {
+            glossaryData = `\ufeff${glossaryEntryList.map((element) => element.join('=')).join('\r\n')}`;
+            break;
+          }
+          case 'QuickTranslate': {
+            glossaryData = `\ufeff${glossaryEntryList.toSorted((a, b) => b[2] - a[2] || a[0].localeCompare(b[0])).map((element) => element.join('=')).join('\r\n')}`;
+            break;
+          }
+          case 'Sáng Tác Việt': {
+            glossaryData = glossaryEntryList.toSorted().toSorted((a, b) => a[2] - b[2]).map((element) => element.join('=')).join('\n');
+            break;
+          }
+          default: {
+            glossaryData = glossaryEntryList.map((element) => element.join('=')).join('\n');
+            break;
+          }
+        }
 
-      glossaryEntryList = null;
-      glossaryDataCopyButton.removeClass('disabled');
-      $downloadButton.attr('href', URL.createObjectURL(new Blob([glossaryData], { type: `${$glossaryTypeSelect.val()}; charset=UTF-8` })));
-      $downloadButton.attr('download', `${$glossaryName.val().length > 0 ? $glossaryName.val() : $glossaryName.attr('placeholder')}.${$glossaryExtension.text()}`);
-      $downloadButton.removeClass('disabled');
-    });
+        $glossaryExtension.text('txt');
+        break;
+      }
+      default: {
+        glossaryData = glossaryEntryList.toSorted((a, b) => b[3] - a[3] || a[1].localeCompare(b[1], 'vi', { ignorePunctuation: true }) || a[0].localeCompare(b[0], 'vi', { ignorePunctuation: true })).map((element) => element.join('\t')).join('\n');
+        $glossaryExtension.text('tsv');
+        break;
+      }
+    }
+
+    glossaryEntryList = null;
+    glossaryDataCopyButton.removeClass('disabled');
+    $downloadButton.attr('href', URL.createObjectURL(new Blob([glossaryData], { type: `${$glossaryTypeSelect.val()}; charset=UTF-8` })));
+    $downloadButton.attr('download', `${$glossaryName.val().length > 0 ? $glossaryName.val() : $glossaryName.attr('placeholder')}.${$glossaryExtension.text()}`);
+    $downloadButton.removeClass('disabled');
   } else {
     glossaryData = '';
     $downloadButton.removeAttr('href');
@@ -1073,7 +1070,7 @@ function reloadGlossaryEntries() {
 
   $('#glossary-entry-counter').text(glossaryLength);
   if (isLoaded) $inputTextarea.trigger('input');
-  if (['vietPhrase', 'name'].every((element) => glossaryList !== element)) glossaryStorage[glossaryList] = glossaryLength < 5000 ? glossaryList : {};
+  if (['vietPhrase', 'name'].every((element) => selectGlossaryList !== element)) glossaryStorage[selectGlossaryList] = glossaryLength < 5000 ? glossaryList : {};
   localStorage.setItem('glossary', JSON.stringify(glossaryStorage));
   $glossaryInput.val(null);
 }
