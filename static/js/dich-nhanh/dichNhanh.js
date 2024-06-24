8e30cced41c3035c86e27635d20d8c30215980f6
Đỗ Hoài Nam <38395309+DoHoaiNam914@users.noreply.github.com>
2024-06-24 13:27:18 +0700

    Update dichNhanh.js

--- static/js/dich-nhanh/dichNhanh.js
+++ static/js/dich-nhanh/dichNhanh.js
@@ -1729,7 +1729,6 @@ $resetButton.on('click', () => {
 $('#glossary-modal').on('shown.bs.modal', () => {
   if ($sourceEntryInput.val().length === 0) return;
   $sourceEntryInput.trigger('input');
-  $sourceEntryInput.autocomplete('close');
 });
 
 $('#glossary-modal').on('hide.bs.modal', () => {
