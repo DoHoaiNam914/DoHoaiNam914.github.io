bebe31c68f290ba254dd6fe75eac6eecabae037a
Đỗ Hoài Nam <38395309+DoHoaiNam914@users.noreply.github.com>
2024-06-27 20:05:57 +0700

    Update dichNhanh.js

--- static/js/dich-nhanh/dichNhanh.js
+++ static/js/dich-nhanh/dichNhanh.js
@@ -1933,7 +1933,6 @@ $translateEntryButtons.click(async function onClick() {
 $addButton.click(() => {
   if ($sourceEntryInput.val().length === 0) return;
   if ($glossaryListSelect.val() === 'namePhu' && Object.hasOwn(glossary.name, $sourceEntryInput.val().trim())) delete glossary.name[$sourceEntryInput.val().trim()];
-  if ($glossaryListSelect.val() === 'vietPhrasePhu' && Object.hasOwn(glossary.vietPhrase, $sourceEntryInput.val().trim())) delete glossary.vietPhrase[$sourceEntryInput.val().trim()];
   if (Object.hasOwn(glossary[$glossaryListSelect.val()], $sourceEntryInput.val().trim())) delete glossary[$glossaryListSelect.val()][$sourceEntryInput.val().trim()];
   glossary[$glossaryListSelect.val()][$sourceEntryInput.val().trim()] = $targetEntryTextarea.val().trim();
   reloadGlossaryEntries();
