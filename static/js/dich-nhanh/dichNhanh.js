937db862a1f00e804023c3cd2f9d7fe0fb7532f7
Đỗ Hoài Nam <38395309+DoHoaiNam914@users.noreply.github.com>
2024-08-29 16:06:57 +0700

    Update dichNhanh.js

--- static/js/dich-nhanh/dichNhanh.js
+++ static/js/dich-nhanh/dichNhanh.js
@@ -666,7 +666,7 @@ const polishTranslation = async function polishTranslationWithArtificialIntellig
               role: 'model',
               parts: [
                 {
-                  text: 'Dịch văn bản (bao gồm cả đầu đề) trong nhãn <TEXT> sang tiếng Việt. Tham khảo tên riêng trong nhãn <NAMES> nếu có nhãn này. Tham khảo ngữ nghĩa theo bản dịch thô trong nhãn <RAW>. Các bản dịch của bạn phải truyền đạt đầy đủ nội dung của văn bản gốc và không được gộp hay cắt dòng. Đảm bảo rằng văn bản dịch tự nhiên cho người bản địa, ngữ pháp chính xác và lựa chọn từ ngữ đúng đắn. Bản dịch của bạn chỉ chứa văn bản đã dịch không bao gồm nhãn và không cần định dạng kiểu chữ.',
+                  text: 'Dịch văn bản (bao gồm cả đầu đề) trong nhãn <TEXT> sang tiếng Việt. Tham khảo tên riêng trong nhãn <NAMES> nếu có nhãn này. Tham khảo ngữ nghĩa theo bản dịch thô trong nhãn <RAW>. Các bản dịch của bạn phải truyền đạt đầy đủ nội dung của văn bản gốc. Không được gộp hay cắt dòng mà phải giữ nguyên số dòng như văn bản gốc. Đảm bảo rằng văn bản dịch tự nhiên cho người bản địa, ngữ pháp chính xác và lựa chọn từ ngữ đúng đắn. Bản dịch của bạn chỉ chứa văn bản đã dịch không bao gồm nhãn và không cần định dạng kiểu chữ.',
                 },
               ],
             },
