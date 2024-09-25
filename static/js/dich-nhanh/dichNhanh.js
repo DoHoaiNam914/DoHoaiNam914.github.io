26125cadd7ea89d0f7bcd1912469b01f86244a72
Đỗ Hoài Nam <dohoainam1409@gmail.com>
2024-09-25 23:32:54 +0700

    Update dichNhanh.js and GoogleGemini.js

--- static/js/dich-nhanh/dichNhanh.js
+++ static/js/dich-nhanh/dichNhanh.js
@@ -702,7 +702,7 @@ const polishTranslation = async function polishTranslationWithArtificialIntellig
               role: 'user',
               parts: [
                 {
-                  text: 'Translate the text within the <TEXT> tag into Vietnamese. Use the name in the <NAMES> tag and the term in the <GLOSSARY> tag. Consider the meaning according to the literal translation in the <RAW> tag. Your translations must convey all the content in the original text and cannot involve explanations or other unnecessary information. Do not merge or cut lines. Keep the same number of lines as the original text. Please ensure that the translated text is natural for native speakers with correct grammar and proper word choices. Your output must only contain the translated text without formatting in the tag and cannot include explanations or other information.',
+                  text: 'Translate the text within the <TEXT> tag into Vietnamese. Use the name in the <NAMES> tag and the term in the <GLOSSARY> tag. Consider the meaning according to the literal translation in the <RAW> tag. Your translations must convey all the content in the original text and cannot involve explanations or other unnecessary information. Do not merge or cut lines. Keep the same number of lines as the original text. Please ensure that the translated text is natural for native speakers with correct grammar and proper word choices. Your output must only contain the translated text without formatting or the tag and cannot include explanations or other information.',
                 },
               ],
             },
