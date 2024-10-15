945f44d39641e4c60ddf126839b4bcef927ec1df
Đỗ Hoài Nam <38395309+DoHoaiNam914@users.noreply.github.com>
2024-10-15 22:03:45 +0700

    Update GoogleGemini.js

--- static/js/dich-nhanh/may-dich/GoogleGemini.js
+++ static/js/dich-nhanh/may-dich/GoogleGemini.js
@@ -220,7 +220,7 @@ class GoogleGemini extends Translator {
               role: 'user',
               parts: [
                 {
-                  text: `Translate the text in #original-text tag into ${targetLanguage}. Don't forget to include the title when translating. Remember to use the names listed under the #name-dictionary tag and the terms from the #glossary tag to enhance the accuracy of translation. Make sure to reference the raw translation in #raw-translation for accuracy and consistency. Your translations must convey all the content in the original text in and cannot involve explanations or other unnecessary information. Please ensure that the translated text is natural for native speakers with correct grammar and proper word choices. Your output must only contain the translated text and cannot include explanations or other information. Please do not format the output.`,
+                  text: `Translate the text in ORIGINAL TEXT into ${targetLanguage}. Use the names listed in NAME DICTIONARY and the terms listed in GLOSSARY to enhance the accuracy of the translation. Your translations must convey all the content in the original text in and cannot involve explanations or other unnecessary information. Please ensure that the translated text is natural for native speakers with correct grammar and proper word choices. Your output must only contain the translated text and cannot include explanations or other information.`,
                 },
               ],
             },
@@ -228,7 +228,7 @@ class GoogleGemini extends Translator {
               role: 'model',
               parts: [
                 {
-                  text: `Please provide the text you would like to have translated into ${targetLanguage} in #original-text.`,
+                  text: `Please provide the text you would like to have translated into ${targetLanguage} within the ORIGINAL TEXT.`,
                 },
               ],
             },
@@ -236,11 +236,20 @@ class GoogleGemini extends Translator {
               role: 'user',
               parts: [
                 {
-                  text: `<script type="text/tab-separated-values" id="glossary">${terminologies.length > 0 ? ['source\ttarget', ...terminologies.map((element) => element.join('\t'))].join('\n') : ''}</script>
-<script type="text/tab-separated-values" id="name-dictionary">${names.length > 0 ? ['source\ttarget', ...names.map((element) => element.join('\t'))].join('\n') : ''}</script>
-<script type="text/plain" id="original-text">
+                  text: `GLOSSARY:
+\`\`\`tsv
+source\ttarget
+${terminologies.length > 0 ? terminologies.map((element) => element.join('\t')).join('\n') : '...'}
+\`\`\`
+NAME DICTIONARY:
+\`\`\`tsv
+source\ttarget
+${terminologies.length > 0 ? names.map((element) => element.join('\t')).join('\n') : '...'}
+\`\`\`
+ORIGINAL TEXT:
+\`\`\`txt
 ${lines.map((element) => element.replace(/^\s+/g, '')).join('\n')}
-</script>`,
+\`\`\``,
                 },
               ],
             },
