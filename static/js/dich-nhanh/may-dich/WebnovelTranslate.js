6a47063af9bd6c9554aa5a4ff8b5d5bdc96989f1
Đỗ Hoài Nam <38395309+DoHoaiNam914@users.noreply.github.com>
2024-10-27 21:28:17 +0700

    Update WebnovelTranslate.js

--- static/js/dich-nhanh/may-dich/WebnovelTranslate.js
+++ static/js/dich-nhanh/may-dich/WebnovelTranslate.js
@@ -1972,7 +1972,7 @@ class WebnovelTranslate extends Translator {
       const EOL = isCj ? '||||' : '\\n';
       const lines = text.split('\n');
       let query = lines.filter((element) => element.replace(/^\s+/, '').length > 0).map((element) => `${isCj ? '\u3000\u3000' : ''}${element}`).join(EOL);
-      query = (isCj ? query.replace(EOL, EOL.repeat(2)) : query).split(new RegExp(`(?:\\.{3}|[${!isCj ? '!,.:;?' : ''}…${isCj ? '、。！，：；？' : ''}]${isCj ? '\\s*' : ''})()`));
+      query = (isCj ? query.replace(EOL, EOL.repeat(2)) : query).split(new RegExp(`(?<=\\.{3}|[${!isCj ? '!,.:;?' : ''}…${isCj ? '、。！，：；？' : ''}](?:[^${!isCj ? '!,.:;?' : ''}…${isCj ? '、。！，：；？' : ''}]*$${isCj ? '|\\s*' : ''}|))`));
       let request = [];
       const responses = [];
 
