66d35189d442f4cecec38e91f474af9534fcc8ab
Đỗ Hoài Nam <38395309+DoHoaiNam914@users.noreply.github.com>
2024-10-27 12:15:16 +0700

    Update WebnovelTranslate.js

--- static/js/dich-nhanh/may-dich/WebnovelTranslate.js
+++ static/js/dich-nhanh/may-dich/WebnovelTranslate.js
@@ -1984,7 +1984,7 @@ class WebnovelTranslate extends Translator {
           requestLines = requestLines.map((element) => `${isCj ? '\u3000\u3000' : ''}${element}`).join(EOL);
           responses.push($.ajax({
             method: 'GET',
-            url: `http://translate.google.com/translate_a/single?client=${this.clientName}&ie=UTF-8&oe=UTF-8&dt=bd&dt=ex&dt=ld&dt=md&dt=rw&dt=rm&dt=ss&dt=t&dt=at&dt=gt&dt=qc&sl=${sourceLanguage}&tl=${targetLanguage}&hl=${targetLanguage}&q=${encodeURIComponent(isCj && responses.length === 0 ? requestLines.replace(EOL, EOL.repeat(2)) : requestLines)}`,
+            url: `${Utils.CORS_PROXY}http://translate.google.com/translate_a/single?client=${this.clientName}&ie=UTF-8&oe=UTF-8&dt=bd&dt=ex&dt=ld&dt=md&dt=rw&dt=rm&dt=ss&dt=t&dt=at&dt=gt&dt=qc&sl=${sourceLanguage}&tl=${targetLanguage}&hl=${targetLanguage}&q=${encodeURIComponent(isCj && responses.length === 0 ? requestLines.replace(EOL, EOL.repeat(2)) : requestLines)}`,
           }));
           requestLines = [];
         }
