cea9ae91a19e57a3284bc371845954ae607316f5
Đỗ Hoài Nam <38395309+DoHoaiNam914@users.noreply.github.com>
2024-11-19 11:01:24 +0700

    Update Gemini.js

--- static/js/dich-nhanh/may-dich/Gemini.js
+++ static/js/dich-nhanh/may-dich/Gemini.js
@@ -112,7 +112,7 @@ ${filteredNomenclature.map((element) => element.join('\t')).join('\n')}
       }
 
       response = response.candidates[0].content.parts[0].text.replace(/ \n$/, '').replaceAll(/(?:^`{3}txt\n|\n`{3}$)/g, '');
-      response = response.split(response.match(/\n{2}/).length <= query.match(/\n{1}/).length ? '\n\n' : '\n');
+    response = response.split(response.match(/\n{2}/) != null && response.match(/\n{2}/).length <= query.match(/\n/).length ? '\n\n' : '\n');
       response = Object.fromEntries(lines.map((element, index) => (element.replace(/^\s+/, '').length > 0 ? index : null)).filter((element) => element != null).map((element, index) => [element, response[index]]));
       this.result = lines.map((element, index) => (response[index] != null ? element.match(/^\s*/)[0].concat(response[index].replace(/^\s+/, '')) : element)).join('\n');
       super.translateText(text, targetLanguage, this.DefaultLanguage.SOURCE_LANGUAGE);
