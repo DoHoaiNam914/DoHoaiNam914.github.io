da03fd1ee4567e0d2d1363d35e28f8c726885c10
Đỗ Hoài Nam <38395309+DoHoaiNam914@users.noreply.github.com>
2025-01-04 15:30:38 +0700

    Hmmmmm

--- static/js/dich-nhanh/may-dich/GenerativeAi.js
+++ static/js/dich-nhanh/may-dich/GenerativeAi.js
@@ -5,7 +5,6 @@ import * as Utils from '/static/js/Utils.js';
 import Anthropic from 'https://esm.run/@anthropic-ai/sdk';
 import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from 'https://esm.run/@google/generative-ai';
 import { HfInference } from 'https://esm.run/@huggingface/inference';
-import { encodingForModel } from 'https://esm.run/js-tiktoken';
 import { Mistral } from 'https://esm.run/@mistralai/mistralai';
 import OpenAI from 'https://esm.run/openai';
 export default class GenerativeAi extends Translator {
@@ -106,7 +105,7 @@ export default class GenerativeAi extends Translator {
                 else if (['gpt-4o-2024-05-13', 'gpt-4-turbo', 'gpt-4-turbo-2024-04-09', 'gpt-4-turbo-preview', 'gpt-4-0125-preview', 'gpt-4-1106-preview', 'gpt-3.5-turbo-0125', 'gpt-3.5-turbo', 'gpt-3.5-turbo-1106', 'gpt-3.5-turbo-instruct'].some(element => model === element))
                     maxCompletionTokens = 4096;
                 else if (['gpt-4', 'gpt-4-0613', 'gpt-4-0314'].some(element => model === element))
-                    maxCompletionTokens = 8192 - encodingForModel(model).encode(`${instructions}${message}`).length;
+                    maxCompletionTokens = undefined; // 8192
                 break;
         }
         requestBody.messages = [
@@ -243,9 +242,9 @@ export default class GenerativeAi extends Translator {
             }
         ];
         if (['meta-llama/Llama-3.2-3B-Instruct', 'google/gemma-2-9b-it', 'meta-llama/Llama-3.2-1B-Instruct', 'microsoft/Phi-3-mini-4k-instruct'].some(element => model === element))
-            chatCompletionInput.max_tokens = 4096 - encodingForModel('gpt-4').encode(model.startsWith('meta-llama') ? `<|begin_of_text|>\n${chatCompletionInput.messages.map(({ content, role }) => `<|start_header_id|>${role}<|end_header_id|>\n\n${content}\n<|eot_id|>`).join('\n')}` : chatCompletionInput.messages.map(({ content, role }) => `${role}\n${content}`).join('\n')).length;
+            chatCompletionInput.max_tokens = undefined; // 4096
         else if (model.startsWith('google') || model.startsWith('meta-llama'))
-            chatCompletionInput.max_tokens = chatCompletionInput.max_tokens - encodingForModel('gpt-4').encode(model.startsWith('meta-llama') ? `<|begin_of_text|>\n${chatCompletionInput.messages.map(({ content, role }) => `<|start_header_id|>${role}<|end_header_id|>\n\n${content}\n<|eot_id|>`).join('\n')}` : chatCompletionInput.messages.map(({ content, role }) => `${role}\n${content}`).join('\n')).length;
+            chatCompletionInput.max_tokens = undefined;
         chatCompletionInput.temperature = 0.3;
         chatCompletionInput.top_p = 0.3;
         chatCompletionInput.model = model;
