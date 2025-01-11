148dce2f7b9dbf35bd58e0184cfd6ab491416fb4
Đỗ Hoài Nam <dohoainam1409@gmail.com>
2025-01-11 16:54:45 +0700

    Update index.html, GenerativeAi.js and one other file

--- static/js/dich-nhanh/may-dich/GenerativeAi.js
+++ static/js/dich-nhanh/may-dich/GenerativeAi.js
@@ -237,7 +237,8 @@ export default class GenerativeAi extends Translator {
             top_p: 0.7
         };
         const { model, temperature, maxTokens, topP } = options;
-        chatCompletionInput.max_tokens = maxTokens > 0 ? maxTokens : (['meta-llama/Llama-3.2-3B-Instruct', 'google/gemma-2-9b-it', 'meta-llama/Llama-3.2-1B-Instruct', 'microsoft/Phi-3-mini-4k-instruct', 'meta-llama/Llama-3.2-11B-Vision-Instruct', 'Qwen/Qwen2-VL-7B-Instruct'].some(element => model === element) ? 4096 : 8192);
+        if (maxTokens > 0)
+            chatCompletionInput.max_tokens = maxTokens;
         chatCompletionInput.messages = [
             ...model.startsWith('google')
                 ? [
