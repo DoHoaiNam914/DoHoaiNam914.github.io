66c3e65b42ea296e46ff7b21f48408a9b9474ddc
Đỗ Hoài Nam <38395309+DoHoaiNam914@users.noreply.github.com>
2025-01-22 16:51:07 +0700

    Update

--- static/js/dich-nhanh/may-dich/GenerativeAi.ts
+++ static/js/dich-nhanh/may-dich/GenerativeAi.ts
@@ -117,7 +117,7 @@ export default class GenerativeAi extends Translator {
     requestBody.messages = [
       {
         content: promptInstructions,
-        role: model.startsWith('o1') ? 'developer' : 'system'
+        role: model.startsWith('o1') ? (model === 'o1-mini' ? 'user' : 'developer') : 'system'
       },
       {
         content: message,
