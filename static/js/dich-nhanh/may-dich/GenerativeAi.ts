2ffb0d8915db632bbf5b433e59b40b7759146970
Đỗ Hoài Nam <38395309+DoHoaiNam914@users.noreply.github.com>
2025-02-27 20:05:26 +0700

    Update GenerativeAi.ts

--- static/js/dich-nhanh/may-dich/GenerativeAi.ts
+++ static/js/dich-nhanh/may-dich/GenerativeAi.ts
@@ -89,7 +89,7 @@ export default class GenerativeAi extends Translator {
         'air-user-id': this.AIR_USER_ID
       },
       signal: this.controller.signal
-    }).then(response => requestBody.stream === true ? (response.data as string).split('\n').filter(element => element.startsWith('data: ') && !element.startsWith('data: [DONE]')).map(element => JSON.parse(`{${element.replace('data: ', '"data":')}}`).data).filter(element => requestBody.n == null || element.choices[0].index === requestBody.n - 1).map(element => element.choices[0].delta.content).filter(element => element != null).join('') : response.data.choices[requestBody.n != null ? requestBody.n - 1 : 0].message.content).catch(error => {
+    }).then(response => requestBody.stream === true ? (response.data as string).split('\n').filter(element => element.startsWith('data: ') && !element.startsWith('data: [DONE]')).map(element => JSON.parse(`{${element.replace('data: ', '"data":')}}`).data).filter(element => requestBody.n == null || requestBody.model === 'o3-mini' || element.choices[0].index === requestBody.n - 1).map(element => element.choices[0].delta.content).filter(element => element != null).join('') : response.data.choices[requestBody.n != null ? requestBody.n - 1 : 0].message.content).catch(error => {
       throw new Error(error.data)
     })
     return response
@@ -155,7 +155,7 @@ export default class GenerativeAi extends Translator {
       if (requestBody.stream as boolean) {
         const collectedMessages: string[] = []
         for await (const chunk of response) {
-          if (requestBody.n == null || chunk.choices[0].index === requestBody.n - 1) collectedMessages.push(chunk.choices[0].delta.content)
+          if (requestBody.n == null || requestBody.model === 'o3-mini' || chunk.choices[0].index === requestBody.n - 1) collectedMessages.push(chunk.choices[0].delta.content)
         }
         return collectedMessages.filter(element => element != null).join('')
       } else {
