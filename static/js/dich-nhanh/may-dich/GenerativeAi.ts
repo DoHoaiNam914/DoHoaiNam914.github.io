e819c5527d7a624932b3a89e395159e270640c70
Đỗ Hoài Nam <38395309+DoHoaiNam914@users.noreply.github.com>
2025-03-05 06:26:54 +0700

    Update GenerativeAi.ts

--- static/js/dich-nhanh/may-dich/GenerativeAi.ts
+++ static/js/dich-nhanh/may-dich/GenerativeAi.ts
@@ -244,15 +244,16 @@ export default class GenerativeAi extends Translator {
     ]
     body.max_tokens = undefined
     body.system = systemInstructions[0]
-    body.temperature = temperature
     if (/-thinking$/.test(model)) {
       body.thinking = {
         type: 'enabled',
         budget_tokens: 16000
       }
+    } else {
+      body.temperature = temperature
+      body.top_k = topK
+      body.top_p = topP
     }
-    body.top_k = topK
-    body.top_p = topP
     const collectedTexts: string[] = []
     await this.anthropic.messages.stream(body, { signal: this.controller.signal }).on('text', text => {
       collectedTexts.push(text)
