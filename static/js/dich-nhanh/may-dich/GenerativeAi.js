2f864b18cffaca000403c4a029937eac04dd147d
Đỗ Hoài Nam <38395309+DoHoaiNam914@users.noreply.github.com>
2025-03-05 13:08:43 +0700

    Update GenerativeAi.js

--- static/js/dich-nhanh/may-dich/GenerativeAi.js
+++ static/js/dich-nhanh/may-dich/GenerativeAi.js
@@ -134,15 +134,14 @@ export default class GenerativeAi extends Translator {
                 }
             ];
             requestBody.model = model.replace(/-(?:low|medium|high)$/, '');
-            if (/^(?:o1|o3-mini).*-(?:low|medium|high)$/.test(message))
+            if (/^(?:o1|o3-mini).*-(?:low|medium|high)$/.test(model))
                 requestBody.reasoning_effort = model.match(/-([^-]+)$/)[1];
             if (Object.hasOwn(requestBody, 'max_completion_tokens'))
                 requestBody.max_completion_tokens = null;
             requestBody.seed = 1234;
-            if (!/\n\s*[^\s]+/.test(message) && ['chatgpt-4o-latest', 'o3-mini'].every(element => requestBody.model !== element))
+            if (!/\n\s*[^\s]+/.test(message) && ['chatgpt-4o-latest', 'o1', 'o3-mini'].every(element => requestBody.model !== element))
                 requestBody.n = 5;
-            if (model !== 'o1')
-                requestBody.stream = true;
+            requestBody.stream = true;
             if (Object.hasOwn(requestBody, 'temperature'))
                 requestBody.temperature = temperature;
             if (Object.hasOwn(requestBody, 'top_p'))
