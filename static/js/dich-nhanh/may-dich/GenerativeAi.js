c59fb76c06266e32af3cc619829e747401b136a5
Đỗ Hoài Nam <38395309+DoHoaiNam914@users.noreply.github.com>
2025-03-05 12:12:02 +0700

    Update GenerativeAi.js

--- static/js/dich-nhanh/may-dich/GenerativeAi.js
+++ static/js/dich-nhanh/may-dich/GenerativeAi.js
@@ -133,7 +133,9 @@ export default class GenerativeAi extends Translator {
                     role: 'user'
                 }
             ];
-            requestBody.model = model;
+            requestBody.model = model.replace(/-(?:low|medium|high)$/, '');
+            if (/^(?:o1|o3-mini).*-(?:low|medium|high)$/.test(message))
+                requestBody.reasoning_effort = model.match(/-([^-]+)$/)[1];
             if (Object.hasOwn(requestBody, 'max_completion_tokens'))
                 requestBody.max_completion_tokens = null;
             requestBody.seed = 1234;
