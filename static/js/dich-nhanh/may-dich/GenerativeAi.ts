c874d4fcd40c94469fffeb98a793375acf2f737a
Đỗ Hoài Nam <38395309+DoHoaiNam914@users.noreply.github.com>
2025-03-15 07:28:51 +0700

    Update GenerativeAi.ts

--- static/js/dich-nhanh/may-dich/GenerativeAi.ts
+++ static/js/dich-nhanh/may-dich/GenerativeAi.ts
@@ -146,7 +146,7 @@ export default class GenerativeAi extends Translator {
       }
       if (Object.hasOwn(requestBody, 'max_completion_tokens')) requestBody.max_completion_tokens = null
       if (/^(?:o1|o3-mini).*-(?:low|medium|high)$/.test(model)) requestBody.reasoning_effort = model.match(/-([^-]+)$/)[1]
-      if (/^(?:o1-mini|gpt-(?:4(?:-0613|o(?:-mini)?-search)?|3.5-turbo-instruct))/.test(requestBody.model) && (requestBody.messages[0].content as string).includes('uuid')) requestBody.response_format = { type: 'json_object' }
+      if (!/^(?:o1-mini|gpt-(?:4(?:-0613|o(?:-mini)?-search)?|3.5-turbo-instruct))/.test(requestBody.model) && (requestBody.messages[0].content as string).includes('uuid')) requestBody.response_format = { type: 'json_object' }
       requestBody.stream = true
       if (/^gpt-4o(?:-mini)?-search/.test(requestBody.model)) {
         requestBody.temperature = null
