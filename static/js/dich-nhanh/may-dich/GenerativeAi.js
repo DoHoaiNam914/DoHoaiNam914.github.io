3a3492541400c8ac043cc21004f2ad945c1469f0
Đỗ Hoài Nam <38395309+DoHoaiNam914@users.noreply.github.com>
2025-03-19 14:33:09 +0700

    Update GenerativeAi.js

--- static/js/dich-nhanh/may-dich/GenerativeAi.js
+++ static/js/dich-nhanh/may-dich/GenerativeAi.js
@@ -441,7 +441,7 @@ export default class GenerativeAi extends Translator {
         });
         if (model.toLowerCase().includes('deepseek-r1'))
             result.replace(/<think>\n(?:.+\n+)+<\/think>\n{2}/, '');
-        if (systemPrompt === 'Professional') {
+        if (systemPrompt === 'Professional' && /(?:^|\n)[a-z0-9#]{12}: /.test(result)) {
             const translationMap = Object.fromEntries(result.split('\n').map(element => element.split(/(^[a-z0-9#]{12}): /).slice(1)));
             result = queryText.map(element => translationMap[element.match(/^[a-z0-9#]{12}/)[0]] ?? '').join('\n');
         }
