97f1f1f66ab35bda0392621b628e5faae82bef18
Đỗ Hoài Nam <38395309+DoHoaiNam914@users.noreply.github.com>
2025-03-23 02:57:53 +0700

    Update GenerativeAi.js

--- static/js/dich-nhanh/may-dich/GenerativeAi.js
+++ static/js/dich-nhanh/may-dich/GenerativeAi.js
@@ -341,7 +341,7 @@ export default class GenerativeAi extends Translator {
         request.temperature = temperature;
         request.top_p = topP;
         request.top_k = topK;
-        const completion = await this.openrouter.chat.completions.create(request);
+        const completion = await this.openrouter.chat.completions.create(request, { signal: this.controller.signal });
         const collectedMessages = [];
         for await (const chunk of completion) {
             collectedMessages.push(chunk.choices[0].delta.content);
