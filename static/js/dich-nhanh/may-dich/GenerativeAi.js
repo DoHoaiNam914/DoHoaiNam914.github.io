09099718b04f4cd6872be0976a670e96199c9521
Đỗ Hoài Nam <38395309+DoHoaiNam914@users.noreply.github.com>
2025-05-13 16:17:37 +0700

    Update GenerativeAi.js

--- static/js/dich-nhanh/may-dich/GenerativeAi.js
+++ static/js/dich-nhanh/may-dich/GenerativeAi.js
@@ -573,7 +573,7 @@ export default class GenerativeAi extends Translator {
             catch (error) {
                 console.error('Error processing JSON translation result:', error);
             }
-            if (extractedTranslation.length === 0) {
+            if (extractedTranslation == null) {
                 const uuidPattern = /(?:^|\n)([a-z0-9#]{12}): ?(.*)/g;
                 let match;
                 let foundUuids = false;
@@ -594,7 +594,7 @@ export default class GenerativeAi extends Translator {
                     }
                 }
             }
-            if (extractedTranslation !== null) {
+            if (extractedTranslation != null) {
                 if (typeof extractedTranslation === 'object' && !Array.isArray(extractedTranslation)) {
                     translationMap = extractedTranslation;
                 }
